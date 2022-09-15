d1e37ef0a9d0068ef4f201dfdcaf736050be58fd
Trung Ngo Lam Bao <s3938154@iMac.rmit.edu.vn>
2022-09-15 17:27:46 +0700

    more commit

---
+++ cv.php
@@ -0,0 +1,100 @@
+<?php
+function get_edu_data(){
+    $fp = fopen("./cv.csv","r");
+    $edu = [];
+    $index = 1;
+    $i = 0;
+    while($row = fgetcsv($fp)){
+        if($index % 2 != 0){
+            $edu[$i] = ["degree" => $row[1], "school" => $row[2], "starting-year" => $row[3], "ending-year" => $row[4]];
+        }
+        $index ++;
+        $i++;
+    }
+}
+
+function get_exp_data(){
+    $fp = fopen("./cv.csv", "r");
+    $edu = [];
+    $index = 0;
+    $i = 0;
+    while($row = fgetcsv($fp)){
+        if($index % 2 == 0){
+            $edu[$i] = ["work" => $row[1], "place" => $row[2], "starting-year" => $row[3], "ending-year" => $row[4]];
+        }
+        $index ++;
+        $i++;
+    }
+}
+
+function sort_exp($job1, $job2){
+    return $job2["starting-year"] - $job1["starting-year"];
+};
+
+function sort_edu($edu1, $edu2){
+    return $edu2["starting-year"] - $edu1["starting-year"];
+};
+
+
+$edu = get_edu_data();
+$exp = get_exp_data();
+
+uasort($edu,"sort_edu");
+uasort($exp,"sort_exp");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>CV</title>
+</head>
+<body>
+      <main>
+          <div class="info-container">
+              <div class="info-container__img">
+                  <img src="./CV-img.jpeg" alt="CV image">
+              </div>
+              <div class="info-container__about">
+                  <h1 class="info-container__about__name">Ngo Lam Bao Trung</h1>
+                  <h2 class="info-container__about__tel">0834986599</h2>
+                  <h3 class="info-container__about__email">ngolambaotrung@gmail.com</h3>
+              </div>
+          </div>
+        <section class="exp-container">
+            <h1>Experience</h1>
+            <?php foreach($exp as $key => $data){ ?>
+            <div class="exp-container__info">
+                <div>
+                    <h2 class="job-tilte"><?=$data["work"]?></h2>
+                    <p><?=$data["place"]?> <span><?=$data["starting-year"]?>-<?=$data["ending-year"]?></span></p>
+                </div>
+            </div>
+            <?php };?>
+        </section>
+        <section class="edu-container">
+            <h1>Education</h1>
+            <?php foreach($edu as $key => $data){ ?>
+            <div class="exp-container__info">
+                <div>
+                    <h2 class="job-tilte"><?=$data["degree"]?></h2>
+                    <p><?=$data["school"]?> <span><?=$data["starting-year"]?>-<?=$data["ending-year"]?></span></p>
+                </div>
+            </div>
+            <?php };?>
+        </section>
+
+        <div class="controller">
+            <label for="hide-email">Click this to hide email</label>
+            <input type="checkbox" name="" id="hide-email">
+            <label for="hide-phone">Click this to hide email</label>
+            <input type="checkbox" name="" id="hide-phone">
+
+        </div>
+      </main>
+  </body>
+  <script src="./script.js"></script>
+</html>
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var infoContainer = document.querySelector(".info-container__img img");
 
-var hideEmailBtn = document.getElementsById("#")
+var hideEmailBtn = document.getElementById("hide-email");
+var hidePhoneBtn = document.getElementById("hide-phone");
+
+
+var email = document.querySelector(".info-container__about__email");
+var phone = document.querySelector(".info-container__about__tel");
+
 
 infoContainer.addEventListener("mouseover", function(){
     infoContainer.style.borderRadius = "0px";
@@ -10,4 +16,20 @@ infoContainer.addEventListener("mouseout", function(){
     infoContainer.style.borderRadius = "50%";
 })
 
+
+hideEmailBtn.addEventListener("change", function(){
+    if(this.checked){
+        email.style.display = "none";
+    }else{
+        email.style.display = "block";
+    }
+})
+
+hidePhoneBtn.addEventListener("change", function(){
+    if(this.checked){
+        phone.style.display = "none";
+    }else{
+        phone.style.display = "block";
+    }
+})
 
