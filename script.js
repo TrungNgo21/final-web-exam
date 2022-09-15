73b6004ff3a231962223b4dc07e5ed7861a68aba
Trung Ngo Lam Bao <s3938154@iMac.rmit.edu.vn>
2022-09-15 16:48:06 +0700

    hihi

---
+++ script.js
@@ -0,0 +1,13 @@
+var infoContainer = document.querySelector(".info-container__img img");
+
+var hideEmailBtn = document.getElementsById("#")
+
+infoContainer.addEventListener("mouseover", function(){
+    infoContainer.style.borderRadius = "0px";
+})
+
+infoContainer.addEventListener("mouseout", function(){
+    infoContainer.style.borderRadius = "50%";
+})
+
+
